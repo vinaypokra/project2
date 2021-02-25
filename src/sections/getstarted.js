c016c958dbcbd533ba0acfc91783bed7e47e4ab0
vinaypokra <79596191+vinaypokra@users.noreply.github.com>
2021-02-25 17:04:56 +0530

    Updated by vinay

--- src/sections/getstarted.js
+++ src/sections/getstarted.js
@@ -1,20 +1,42 @@
-import React, {Component} from 'react';
-import Grid from '@material-ui/core/Grid';
-import Styles from '../app-style.js';
-import { Typography} from '@material-ui/core';
-import {colors} from '../services';
-import Signupform from '../signupform';
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import Styles from "../app-style.js";
+import { Typography } from "@material-ui/core";
+import { colors } from "../services";
+import Signupform from "../signupform";
 class GetStarted extends Component {
-  render(){
+  render() {
     return (
-      <Grid item container direction="column" style={{...Styles.padding5, ...Styles.greyBG, ...Styles.highZ}} alignItems="center" justify="center" className="top">
+      <Grid
+        item
+        container
+        direction="column"
+        style={{ ...Styles.padding5, ...Styles.greyBG, ...Styles.highZ }}
+        alignItems="center"
+        justify="center"
+        className="top"
+      >
         <Grid item>
-          <Typography variant="h2" style={{...Styles.colorWhite, ...{textAlign: 'center'}}}>Join the community of like minded people</Typography>
-          <Typography variant="subtitle1" style={{color: colors.grey, textAlign: 'center'}}>... and unlock a secret, no-equipment home-workout absolutely free of cost</Typography>
-          <hr/>
+          <Typography
+            variant="h2"
+            style={{ ...Styles.colorWhite, ...{ textAlign: "center" } }}
+          >
+            Join the community of like minded people
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            style={{ color: colors.grey, textAlign: "center" }}
+          >
+            ... and unlock a secret, no-equipment home-workout absolutely free
+            of cost
+          </Typography>
+          <hr />
         </Grid>
-        <Grid item style={{...Styles.padding5}}>
-          <Signupform/>
+        <Grid
+          item
+          style={{ ...Styles.padding5, marginBottom: Styles.spacing(8) }}
+        >
+          <Signupform />
         </Grid>
       </Grid>
     );
